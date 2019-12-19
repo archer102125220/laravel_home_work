e98e9688f2295522953902b3b011db5da071fd34
Archer <archer102125220.2015@gmail.com>
2019-12-19 20:32:45 +0800

    model update, route update

--- app/Commemt.php
+++ app/Commemt.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Commemt extends Model
 {
-    //
+    use Notifiable;
+    protected $primarykey = 'comment_id';
+    const UPDATED_AT = null;
+
+    protected $fillable=[
+        'comment_id', 'title', 'content'
+    ];
 }
