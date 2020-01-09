9a6fff4f391a5894a0878c99eb23968b198d4032
Archer <archer102125220.2015@gmail.com>
2020-01-09 23:07:45 +0800

    create Cors middleware

--- routes/api.php
+++ routes/api.php
@@ -12,15 +12,19 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::post('register', 'UserController@register');
-Route::post('login', 'UserController@login');
-Route::group(['middleware' => 'jwtAuth'], function () {
-    Route::get('user/getThisUserData', 'UserController@getThisUserData');
-    Route::match(['put', 'patch'], 'user/edit/{userId}', 'UserController@editUser');
-    Route::post('user/new_user', 'UserController@register');
-    Route::delete('user/delete_user', 'UserController@deleteUser');
+Route::group([
+        'middleware' => ['api', 'cors'],
+        // 'namespace' => $this->namespace,
+    ], function ($router) {
+    Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@login');
+    Route::group(['middleware' => 'jwtAuth'], function () {
+        Route::get('user/getThisUserData', 'UserController@getThisUserData');
+        Route::match(['put', 'patch'], 'user/edit/{userId}', 'UserController@editUser');
+        Route::post('user/new_user', 'UserController@register');
+        Route::delete('user/delete_user', 'UserController@deleteUser');
     
-    Route::post('comment/new_comment', 'commentController@newComment');
-    Route::match(['put', 'patch'], 'comment/edit/{comment_id}', 'commentController@editComment');
+        Route::post('comment/new_comment', 'commentController@newComment');
+        Route::match(['put', 'patch'], 'comment/edit/{comment_id}', 'commentController@editComment');
+    });
 });
