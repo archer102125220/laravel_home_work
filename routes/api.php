938c91c03ea7567372c42a40c22f5967d69c2313
Archer <archer102125220.2015@gmail.com>
2020-01-09 23:21:18 +0800

    Refactoring

--- routes/api.php
+++ routes/api.php
@@ -12,18 +12,19 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group([
-        'middleware' => 'cors',
-    ], function ($router) {
+Route::group(['middleware' => 'cors'], function () {
     Route::post('register', 'UserController@register');
     Route::post('login', 'UserController@login');
     Route::group(['middleware' => 'jwtAuth'], function () {
-        Route::get('user/getThisUserData', 'UserController@getThisUserData');
-        Route::match(['put', 'patch'], 'user/edit/{userId}', 'UserController@editUser');
-        Route::post('user/new_user', 'UserController@register');
-        Route::delete('user/delete_user', 'UserController@deleteUser');
-    
-        Route::post('comment/new_comment', 'commentController@newComment');
-        Route::match(['put', 'patch'], 'comment/edit/{comment_id}', 'commentController@editComment');
+        Route::group([ 'prefix' => 'user',], function () {
+            Route::get('getThisUserData', 'UserController@getThisUserData');
+            Route::match(['put', 'patch'], 'edit/{userId}', 'UserController@editUser');
+            Route::post('new_user', 'UserController@register');
+            Route::delete('delete_user', 'UserController@deleteUser');
+        });
+        Route::group([ 'prefix' => 'comment',], function () {
+            Route::post('new_comment', 'commentController@newComment');
+            Route::match(['put', 'patch'], 'edit/{comment_id}', 'commentController@editComment');
+        });
     });
 });
